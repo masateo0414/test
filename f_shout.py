567a65338707b036864b4fcfe4ffc488d7857c71
masateo0414 <masateo414@gmail.com>
2024-03-14 02:08:03 +0900

    shoutノーマルのテスト実装

---
+++ f_shout.py
@@ -0,0 +1,60 @@
+import random
+
+# 丸の位置セット
+ms_p = ["oxooo","ooxoo","oooxo","oooox",
+        "oxooo","ooxoo","oooxo","oooox",
+        "oxooo","ooxoo","oooxo","oooox",
+        "oxooo","ooxoo","oooxo","oooox",
+        "xoxoo","xooxo",
+        "oxoxo",
+        "oxoooo","ooxooo","oooxoo","ooooxo","ooooox",
+        "oxoooo","ooxooo","oooxoo","ooooxo","ooooox",
+        "oxoooo","ooxooo","oooxoo","ooooxo","ooooox",
+        "oxoooo","ooxooo","oooxoo","ooooxo","ooooox",
+        "xoxooo","xooxoo","xoooxo",
+        "oxoxoo","oxooxo",
+        "ooxoxo",
+        "oxooooo","ooxoooo","oooxooo","ooooxoo","oooooxo","oooooox",
+        "oxooooo","ooxoooo","oooxooo","ooooxoo","oooooxo","oooooox",
+        "oxooooo","ooxoooo","oooxooo","ooooxoo","oooooxo","oooooox",
+        "oxoxooo","oxooxoo","oxoooxo","oxoooox",
+        "ooxoxoo","ooxooxo",
+        "oooxoxo",
+        "oxoooooo","ooxooooo","oooxoooo","ooooxooo","oooooxoo","ooooooxo","ooooooox",
+        "oxoooooo","ooxooooo","oooxoooo","ooooxooo","oooooxoo","ooooooxo","ooooooox",
+        "oxooxooo","oxoooxoo","oxooooxo",
+        "ooxooxoo","ooxoooxo",
+        "oooxooxo"]
+
+ms_ph = ["xoxoo","xooxo","xooox",
+        "oxoxo","oxoox",
+        "ooxox",
+        "xoxooo","xooxoo","xoooxo","xoooox",
+        "oxoxoo","oxooxo","oxooox",
+        "ooxoxo","ooxoox",
+        "oooxox",
+        "xoxoooo","xooxooo","xoooxoo","xooooxo","xooooox",
+        "oxoxooo","oxooxoo","oxoooxo","oxoooox",
+        "ooxoxoo","ooxooxo","ooxooox",
+        "oooxoxo","oooxoox",
+        "ooooxox",
+        "oxoxoooo","oxooxooo","oxoooxoo","oxooooxo","oxooooox",
+        "ooxoxooo","ooxooxoo","ooxoooxo","ooxoooox",
+        "oooxoxoo","oooxooxo","oooxooox",
+        "ooooxoox"]
+
+# ランダム文字セット
+ms_l = "あいいいいいうううううえおかかかかかききききくくくくけけここここさしししししすすすせそたたたたたちつててててとととととななななにににににぬねのののののははははひふへほままみむめもやゆよらりりりるるるれれれれろわわわわんんんんんががぎぎぐげごござざじじずずぜぞだだでででどどばばびぶべぼぼゃゃゅゅょょっっ"
+
+
+def normal():
+    ms_q = ""
+    
+    for p in random.choice(ms_p):
+        if p == "o":
+            ms_q += "○"
+        else:
+            ms_q += random.choice(ms_l)
+    
+    return ms_q
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 import os
+import random
 from discord.ext import commands
+import f_shout
 
 bot = commands.Bot(command_prefix="!!", intents=discord.Intents.all())
 
@@ -16,6 +18,13 @@ async def on_ready():
 @bot.command()
 async def call(ctx):
     await ctx.send("マ！")
+
+#!!shout
+async def shout(ctx,arg):
+    quiz = f_shout.normal()
+
+    await ctx.send(embed=discord.Embed(title=":boom:SPRINT SHOUT", description="**【○に文字を入れて言葉を完成させよ】**\n# {}".format(quiz), color=discord.Colour.green()))
+
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 bot.run(TOKEN)
