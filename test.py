160675a8c6f9ae57852012917f71ef6167a9b9e1
masateo0414 <masateo414@gmail.com>
2024-03-13 23:06:45 +0900

    commit test

---
+++ test.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!!", intents=discord.Intents.all())
+
+
+#起動したときに起こるイベント
+@bot.event
+async def on_ready():
+    print("準備完了")
+    await bot.tree.sync()
+
+
+#!!ping
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+
+bot.run("TOKEN")
