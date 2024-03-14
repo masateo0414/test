655b3d20c09c2f41d2957a73ff057aa6f6851ba0
masateo0414 <masateo414@gmail.com>
2024-03-15 08:25:56 +0900

    喋る機能とmemory追加！

---
+++ f_reply.py
@@ -0,0 +1,79 @@
+import random
+
+aiduti_dic = [ ["おはよ", ["おはよう","おはようございます:sunny:","おはよ！","# 押忍","おやすみ～","あさ！"]],
+               ["こんにちは", ["こんにちは","ども！","# 押忍","げんき！"]],
+               ["こんにちわ", ["こんにちわ","ども！","# 押忍","げんき！"]],
+               ["こんばんは", ["こんばんは","フワ～:yawning_face:","# 押忍","よる！"]],
+               ["こんばんわ", ["こんばんは","フワ～:yawning_face:","# 押忍","よる！"]],
+               ["ありがと", ["どういたしまして","いいよ","エヘ！","ウィ"]],
+               ["ごめん", ["許す","いいよお","# *NO PROBLEM*","許しません"]],
+               ["すまん", ["許す","いいよお","# *NO PROBLEM*","許しません"]],
+               ["すみません", ["許す","いいよお","# *NO PROBLEM*","許しません"]],
+               ["さよなら", ["！？","ばいばい","やだ～","乙した！"]],
+               ["じゃあね", ["！？","ばいばい","やだ～","乙した！"]],
+                ["ちんこ", ["ちんこ！","ちんこ！！","<:chinko:1134001412695674891>","草","そういうのやめよう"]],
+                ["チンコ", ["ちんこ！","ちんこ！！","<:chinko:1134001412695674891>","草","そういうのやめよう"]],
+                ["ちんぽ", ["ちんぽ！","ちんぽ！！","<:chinko:1134001412695674891>","草","そういうのやめよう"]],
+                ["チンポ", ["ちんぽ！","ちんぽ！！","<:chinko:1134001412695674891>","草","そういうのやめよう"]] ]
+
+randomRep_dic = ["ms",
+              "ms！",
+              "ms……",
+              "msを"
+              "msかも",
+              "msです",
+              "msだね",
+              "えーと、ms",
+              "msですか？",
+              "それはms",
+              "# ms"]
+
+randomSay_dic = ["ms",
+                 "ms！",
+                 "ms……",
+                 "msを",
+                 "ms！ms！",
+                 "msかも",
+                 "msです",
+                 "あ～msms"
+                 "あ！msだ",
+                 "やばい　ms",
+                 "msの時間",
+                 "msがきてる",
+                 "# ms"
+                 
+                 "わあ",
+                 "うーん",
+                 "マ",
+                 "あ～",
+                 "# マ！"]
+
+randomSays_dic = ["ms1のms2",
+                  "ms1と、ms2",
+                  "ms1みたいなms2"]
+
+def aiduti(msg):
+    #たまに話を聞かない
+    if random.randrange(10) == 0:
+        return None
+    
+    for list in aiduti_dic:
+        if list[0] in msg:
+            reply = random.choice(list[1])
+            return reply
+    return None
+
+def randomReply(list):
+    meisi = random.choice(list)
+    reply = random.choice(randomRep_dic).replace("ms",meisi)
+    return reply
+
+def randomSay(list):
+    meisi = random.choice(list)
+    meisi2 = random.choice(list)
+
+    if random.randrange(5) > 0:
+        reply = random.choice(randomSay_dic).replace("ms",meisi)
+    else:
+        reply = random.choice(randomSays_dic).replace("ms1",meisi).replace("ms2",meisi2)
+    return reply
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import func
 import f_shout
 import f_dice
 import f_login
+import f_reply
 
 load_dotenv()
 
@@ -38,6 +39,13 @@ workbook = gc.open_by_key(SPREADSHEET_KEY)
 
 bot = commands.Bot(command_prefix="!!", intents=discord.Intents.all())
 
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+#真鯖のguild(未解決)
+global guild
+guild = client.get_guild(1133831716507754536)
 #MasateoのID
 masateo_id = 414755451419230208
 #本当のゲリ
@@ -114,7 +122,7 @@ async def login(ctx):
     login_list = ws_login.col_values(1)
     print(login_list)
 
-    if ctx.author.id in login_list and ctx.author.id != masateo_id:
+    if str(ctx.author.id) in login_list and ctx.author.id != masateo_id:
         embed = discord.Embed(title=":gift:LOGIN BOUNS", description="**<@{}>\n今日のログインボーナスは取得済みです**".format(ctx.author.id), color=0x00f230)
         await ctx.send(embed=embed)         
 
@@ -141,6 +149,21 @@ async def login(ctx):
         ws_login.update_cell(len(login_list)+1, 1, ctx.author.id)
         ws_login.update_cell(len(login_list)+1, 2, ctx.author.name)
 
+#!!memory
+@bot.command()
+async def memory(ctx,*arg):
+    if len(arg) != 1:
+        embed = discord.Embed(title=":memo:TEACH WORD", description=f"`!!memory`と半角スペースのあとに、おぼえさせたい言葉をひとつ入力してください", color=0x3d77ff)
+        await ctx.send(embed=embed)
+    else:
+        ws_reply = workbook.worksheet("reply")
+        mem_list = ws_reply.col_values(2)
+        ws_reply.update_cell(len(mem_list)+1, 2, arg[0])
+
+        embed = discord.Embed(title=":memo:TEACH WORD", description=f":white_check_mark:真鯖botは以下の言葉をおぼえました\n## {arg[0]}", color=0x3d77ff)
+        await ctx.send(embed=embed)
+
+
 #!!login_listreset
 @bot.command()
 async def login_listreset(ctx):
@@ -149,10 +172,41 @@ async def login_listreset(ctx):
 
 #test~
 @bot.command()
-async def dbtest(ctx):
-    worksheet = workbook.sheet1
-    worksheet.batch_clear(["A:B"])
+async def talktest(ctx):
+    ws_reply = workbook.worksheet("reply")
+    meisi_list = ws_reply.col_values(2)
+    await ctx.send(f_reply.randomSay(meisi_list))
 
+
+#返信機能
+@bot.event
+async def on_message(message):
+    if bot.user in message.mentions:
+        ws_reply = workbook.worksheet("reply")
+
+        #特定の言葉に反応する
+        reply = f_reply.aiduti(message.content)
+        #ない場合は適当に喋る
+        if reply == None:
+            reply = randomSpeak(ws_reply)
+
+
+        #送信！
+        await message.reply(reply)
+    
+    #フレームワーク移行のための
+    await bot.process_commands(message)
+
+
+# 適当発言生成
+def randomSpeak(ws):
+    if random.randrange(3) > 0:
+        aiduti_list = ws.col_values(1)
+        reply = aiduti_list[random.randrange(len(aiduti_list))]
+    else:
+        meisi_list = ws.col_values(2)
+        reply = f_reply.randomReply(meisi_list)
+    return reply
 
 # -------------------------------------------------------------------------------------------
 # error syori
@@ -178,11 +232,20 @@ async def loop():
     # 00:00 - login reset
     if old_now != now and now == "00:00":
         await channel.send("login reset")
-        worksheet = workbook.sheet1
+        worksheet = workbook.worksheet("login")
         worksheet.batch_clear(["A:B"])
     
     # 現在時刻更新
     old_now = now
+
+    # 突然喋る
+    if random.randrange(50000) == 0:
+        ws_reply = workbook.worksheet("reply")
+        meisi_list = ws_reply.col_values(2)
+        reply = f_reply.randomSay(meisi_list)
+
+        channel = bot.get_channel(1133837604991811665) #ノーマル雑談
+        await channel.send(reply)
 
 
 TOKEN = os.getenv("DISCORD_TOKEN")
