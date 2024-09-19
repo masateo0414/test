b026a8dc2ab7cf18ef30ae9e744580deeb6dde4f
masateo0414 <masateo414@gmail.com>
2024-09-20 07:01:49 +0900

    喋る頻度さげました

--- main.py
+++ main.py
@@ -721,7 +721,7 @@ async def loop():
     old_now = now
 
     # 突然喋る
-    if random.randrange(20000) == 0:
+    if random.randrange(50000) == 0:
         ws_reply = workbook.worksheet("reply")
         meisi_list = ws_reply.col_values(2)
         randomRep_dic = ws_reply.col_values(3)
